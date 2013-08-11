ad2b2fbe1a87dd6630700280fd772ce715e6d9df
Srinivas Aluri <Srinivas.Aluria@wellogic.com>
2013-08-12 01:50:38 +0530

    created Hello.java class with two sysouts and tested with junit test class

---
+++ src/main/gsmsc/Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+	
+public static void main(String args[])
+	{
+		
+System.out.println("Hello this is Sample java program");
+
+System.out.println("added sysout as per comments");	
+}
+
+}
